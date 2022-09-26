e44770bc6046f1bb3a397cff9401ef7bcb98b31e
Kathan Patel <kathanpatel@cmail.carleton.ca>
2022-09-26 13:12:40 -0400

    added addBuddy and removeBuddy methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook() {
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        this.buddies.add(buddy);
+        System.out.println(buddy.getName() + " added to address book");
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.buddies.remove(buddy);
+        System.out.println(buddy.getName() + " removed from the address book");
+    }
+
     public static void main(String[] args) {
+
         System.out.println("AddressBook");
     }
 }
