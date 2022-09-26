608d3bcdda4c2b2da90fc77cc913955316c88823
Kathan Patel <kathanpatel@cmail.carleton.ca>
2022-09-26 13:17:34 -0400

    Changed logic for adding and removing buddy from address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -8,17 +8,23 @@ public class AddressBook {
     }
 
     public void addBuddy(BuddyInfo buddy){
-        this.buddies.add(buddy);
-        System.out.println(buddy.getName() + " added to address book");
+        if(buddy != null) {
+            this.buddies.add(buddy);
+            System.out.println(buddy.getName() + " added to address book");
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        this.buddies.remove(buddy);
-        System.out.println(buddy.getName() + " removed from the address book");
+    public void removeBuddy(int index){
+        if(index >= 0 && index < buddies.size()){
+            this.buddies.remove(index);
+            System.out.println(buddies.get(index).getName() + " removed from the address book");
+        }
     }
 
     public static void main(String[] args) {
-
-        System.out.println("AddressBook");
+        BuddyInfo homer = new BuddyInfo("Homer");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(homer);
+        addressBook.removeBuddy(0);
     }
 }
